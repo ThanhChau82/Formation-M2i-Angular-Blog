0d2da78adfb81f4a6def2c9115bc97394b775148
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-11 10:52:10 +0200

    Sprint 1 - Créer l'aborescence de projet blog : 1) Ajouter les composants "list", "insert", "edit", le model "post", le service "post". 2) Paramétrer le routing entre les composants.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditComponent } from './edit/edit.component';
+import { InsertComponent } from './insert/insert.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'list', component : ListComponent},
+  {path : 'insert', component : InsertComponent},
+  {path : 'edit', component : EditComponent}, // à ajouter un paramètre plus tard pour id du post
+  {path : '', redirectTo : 'list', pathMatch : 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { InsertComponent } from './insert/insert.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    InsertComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
