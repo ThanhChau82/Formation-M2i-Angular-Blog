3998d72e7cee47518aca51451d0aeb34ba7203dd
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-11 14:34:08 +0200

    Sprint 6 - Modification d'un post : intercepter l'observable pour récupérer l'id du post à modifier.

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route : ActivatedRoute) { } // ActivatedRoute pour récupérer les paramètres
 
   ngOnInit(): void {
+    this.route.params.subscribe( // inscire aux observables pour intercepter le flux de paramètres
+      parametres => {
+        console.log(parametres['id']);
+      }
+    );
   }
+
 
 }
